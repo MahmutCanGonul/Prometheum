973cb51ef89f6714c6e24c787784fcedfedefd99
Mahmut Can Gönül <75094927+MahmutCanGonul@users.noreply.github.com>
2021-11-27 18:22:14 +0300

    Create Shell.py

---
+++ Shell.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 26 14:13:07 2021
+
+@author: Mahmut Can Gonol
+"""
+
+import Prometheum
+
+
+while True:
+    text = input("prometheum => ")
+    result,error = Prometheum.run('<stdio>',text)
+    
+    if error:
+        print(error.error_message())
+    else:
+        print(result)
+    
